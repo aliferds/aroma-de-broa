60a2cf92ae6c9f7d813aca4d9073c3e86ea751d3
aliferds <aliferduartesilva@gmail.com>
2025-08-28 19:31:19 -0300

    Feat: Carrinho de pedidos

--- src/form/order.js
+++ src/form/order.js
@@ -2,6 +2,7 @@ const btnProduct = document.getElementById('addOnCart');
 const btnReset = document.getElementById('resetCart');
 const productModal = document.getElementById('product_modal');
 
+const elemQuant = document.querySelectorAll('.quantity');
 
 btnProduct.addEventListener('click', (e) => {
   e.preventDefault();
@@ -21,3 +22,69 @@ document.addEventListener('keydown', (e) => {
         productModal.classList.remove('open');
     }
 });
+
+
+elemQuant.forEach(elem => {
+    const btn_remove = elem.children[0];
+    const input = elem.children[1];
+    const btn_add = elem.children[2];
+
+    btn_remove.addEventListener('click', e => {
+        e.preventDefault();
+        // console.log(`Diminuindo 1 de ${input.id}`);
+        if(parseInt(input.getAttribute('value')) > 0){
+            sumQuantity(-1);
+        }
+    });
+
+    function sumQuantity(value){
+        let qtd = parseInt(input.getAttribute('value'));
+        input.setAttribute('value', qtd+value);
+        // console.log(input);
+    }
+
+    btn_add.addEventListener('click', e => {
+        e.preventDefault();
+        // console.log(`Acrescentando 1 em ${input.id}`);
+        sumQuantity(1);
+    });
+})
+
+const btn_prev = productModal.children[0][1];
+const btn_next = productModal.children[0][0].children[1].children[2];
+
+
+// console.log(productModal.children[0][0].children[1].children[2]);
+
+btn_prev.addEventListener('click', e => {
+    e.preventDefault();
+    previousItems();
+})
+btn_next.addEventListener('click', e => {
+    e.preventDefault();
+    nextItems();
+})
+
+const container = productModal.children[0];
+
+function nextItems(){
+    const scrollDistance = document.querySelector('.card_produto').getBoundingClientRect().width;
+    container.scrollBy({left: scrollDistance, behavior: "smooth"});
+    // console.log(scrollDistance);
+}
+function previousItems(){
+    const scrollDistance = document.querySelector('.card_produto').getBoundingClientRect().width;
+    container.scrollBy({left: -scrollDistance, behavior: "smooth"});
+}
+
+const addChart = document.getElementById('submit_chart');
+const cleanChart = document.getElementById('reset_order');
+
+addChart.addEventListener('click', e => {
+    e.preventDefault();
+    // logica para adicionar ao formulário de pedido
+})
+cleanChart.addEventListener('click', e => {
+    e.preventDefault();
+    // lógica para limpar o que foi selecionado
+})
