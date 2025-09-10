f56f06dd97ef12b58412c7037df94ed604b1bcc6
aliferds <aliferduartesilva@gmail.com>
2025-09-10 19:04:55 -0300

    Feat: welcome modal appear just 1 time in sectionstorage

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,23 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const modal = document.createElement('wc-modal');
-  
-  modal.innerHTML = `
-    <style>
-      wc-modal {
-        --wc-modal-container-bg-color: var(--pale_sand);
-      }
-      p {
-        margin-top: 1rem;
-      }
-    </style>
-    <h1>Bem-vindo ao site da padaria Aroma de Broa</h1>
-    <p>
-      Aqui você irá encontrar todos os nossos produtos, informações
-      sobre a padaria e contatos. Tanbém será possível fazer suas compras
-      por aqui.
-    </p>
-  `;
+   // A chave que vamos usar para marcar a visita nesta sessão
+    const hasVisitedSessionKey = 'hasVisitedThisSession';
 
-  document.body.appendChild(modal);
-  modal.open();
+    if (!sessionStorage.getItem(hasVisitedSessionKey)) {
+      const modal = document.createElement('wc-modal');
+      
+      modal.innerHTML = `
+        <style>
+          wc-modal {
+            --wc-modal-container-bg-color: var(--pale_sand);
+          }
+          p {
+            margin-top: 1rem;
+          }
+        </style>
+        <h1>Bem-vindo ao site da padaria Aroma de Broa</h1>
+        <p>
+          Aqui você irá encontrar todos os nossos produtos, informações
+          sobre a padaria e contatos. Tanbém será possível fazer suas compras
+          por aqui.
+        </p>
+      `;
+
+      document.body.appendChild(modal);
+      modal.open();
+      sessionStorage.setItem(hasVisitedSessionKey, 'true');
+  }
 });
